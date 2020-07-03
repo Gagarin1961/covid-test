4501dd5b164f9d87e7bba9d4837d59156c783f73
Artem Platonov <artem.platonov@epita.fr>
2020-07-03 12:19:20 +0200

    Fixed country page not displaying by adding a loading state that prevents page to render before axios finishes the request, also added error handling

--- src/Country.js
+++ src/Country.js
@@ -1,27 +1,62 @@
 import React, {useEffect, useState} from 'react';
 import { useRouteMatch } from 'react-router-dom';
+import axios from "axios";
 
-const Country = ({ api }) => {
+const api = axios.create({
+    baseURL: 'https://api.covid19api.com/',
+    responseType: 'json'
+})
+
+const Country = () => {
     const [countries, setCountries] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState('');
 
     let match = useRouteMatch();
 
     useEffect(() => {
         api.get('/summary').then(res => {
             setCountries(res.data.Countries);
+            setLoading(true);
+        }).catch(err => {
+            setError(err.message);
+            setLoading(true);
         })
     }, [])
 
-    const index = countries.map(el => el.Country).indexOf(match.params.country);
+    if (loading) {
+        const index = countries.map(el => el.Country).indexOf(match.params.country);
+        let country = countries[index];
 
-    let country = countries[index];
+        console.log(country);
 
-    console.log(countries);
-    console.log(country);
-
-    return (
-        <div>{country}</div>
-    )
+        if (error)
+            return (
+                <li>{error.message}</li>
+            )
+        else
+        {
+            return (
+                <div>
+                    {country.Country}
+                    <div>New Confirmed: {country.NewConfirmed}</div>
+                    <div>New Deaths: {country.NewDeaths}</div>
+                    <div>New Recovered: {country.NewRecovered}</div>
+                    <div>Total Confirmed: {country.TotalConfirmed}</div>
+                    <div>Total Deaths: {country.TotalDeaths}</div>
+                    <div>Total Recovered: {country.TotalRecovered}</div>
+                </div>
+            )
+        }
+    }
+    else
+    {
+        return (
+            <div>
+                Loading...
+            </div>
+        )
+    }
 }
 
 export default Country;
